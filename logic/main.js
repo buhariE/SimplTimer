c1d762611768851e2a90bf663b670d80ef568c71
Ebuhari <manuelbuhari@gmail.com>
2025-12-30 21:43:27 -0700

    Complete UI fir wirldclock card:
    - add timezone dropdown button
    - add hour format toggle button
    - add time display with hours, minutes, seconds, and period (AM/PM)

--- logic/main.js
+++ logic/main.js
@@ -1,8 +1,10 @@
 // Retrieve elements from DOM
 const themeToggleWrapper = document.querySelector('.themeToggleWrapper');
 const themeToggleCircle = document.querySelector('.themeToggleCircle');
+const hourToggleCircle = document.querySelector('.hourToggleCircle');
 const contentCardsContainer = document.querySelector('.contentCards');
 let theme = 'dark';
+let hourFormat = '12H';
 const translateXvalue = 345;
 let contentCardMovement = 0;
 let max = 0;
@@ -19,6 +21,17 @@ function toggleTheme() {
         theme = 'dark';
         themeToggleCircle.style.transform = 'translateX(0px)';
         themeToggleCircle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    }
+}
+function toggleFormat() {
+    if (hourFormat === '12H') {
+        hourFormat = '24H';
+        hourToggleCircle.style.transform = 'translateX(23px)';
+        hourToggleCircle.innerHTML = '24H';
+    } else {
+        hourFormat = '12H';
+        hourToggleCircle.style.transform = 'translateX(0px)';
+        hourToggleCircle.innerHTML = '12H';
     }
 }
 
